ed6f7312ed0cecf2192fae8eb79b2003bf71d97d
laplalaplanh <nguyen2697s@gmail.com>
2020-03-25 14:54:26 +0700

    Update RPC_game.py

--- RPC_game.py
+++ RPC_game.py
@@ -1,4 +1,8 @@
-#Rock Paper Scissors Game
+# Rock Paper Scissors Game
+# Make a rock-paper-scissors game where it is the player vs the computer. 
+# The computer’s answer will be randomly generated, while the program will ask the user for their input. 
+# This project will better your understanding of while loops and if statements.
+
 import random
 
 while(True):
